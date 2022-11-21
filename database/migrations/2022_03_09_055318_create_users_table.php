3d947975d3912ce1cd63a4d02bc3d6f971644e07
duongdinhnguyen <dinhduongco1308@gmail.com>
2022-11-21 21:38:53 +0700

    update migration

--- database/migrations/2022_03_09_055318_create_users_table.php
+++ database/migrations/2022_03_09_055318_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->increments('id');
             $table->string('student_id')->unique()->nullable();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('phone')->nullable();
