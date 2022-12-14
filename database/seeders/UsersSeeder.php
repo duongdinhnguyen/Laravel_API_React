8d05a237262ba1790f3d4a8e2f476fba198bc8f0
duonqdinhnquyen <nguyen.dinhduong@jvb-corp.com>
2022-12-14 16:15:02 +0700

    Interval ajax

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -16,16 +16,6 @@ class UsersSeeder extends Seeder
     public function run()
     {
         DB::table('users')
-            ->insert(['student_id' => '18001200', 'name' => 'Nguyễn Thu Thảo', 'email' => 'nguyenthuthao_t63@hus.edu.vn', 'password' => Hash::make('12345678'), 'phone' => '0367778603', 'rule' => '1','google_id' => '106520950925893744265' ]);
-        DB::table('users')
             ->insert(['student_id' => '18001119', 'name' => 'Nguyễn Đình Đương', 'email' => 'nguyendinhduong_t63@hus.edu.vn', 'password' => Hash::make('12345678'), 'phone' => '0367778603', 'rule' => '1','google_id' => '118006738246523608801']);
-        DB::table('users')
-            ->insert(['student_id' => '18001220', 'name' => 'Nguyễn Thành Vinh', 'email' => 'nguyenthanhvinh_t63@hus.edu.vn', 'password' => Hash::make('12345678'), 'phone' => '0367778603', 'rule' => '1' ,'google_id' => '116935558259104789277']);
-        DB::table('users')
-            ->insert(['student_id' => '18001111', 'name' => 'Đương', 'email' => 'nguyendinhduong.jvb@gmail.com', 'password' => Hash::make('12345678'), 'phone' => '0335483369', 'rule' => '0']);
-        DB::table('users')
-            ->insert(['student_id' => '18001001', 'name' => 'Vinh', 'email' => 'nguyenthanhvinh.jvb@gmail.com', 'password' => Hash::make('12345678'), 'phone' => '0367778603', 'rule' => '0']);
-        DB::table('users')
-            ->insert(['student_id' => '18001024', 'name' => 'Thảo', 'email' => 'nguyenthuthao.jvb@gmail.com', 'password' => Hash::make('12345678'), 'phone' => '0367778603', 'rule' => '0', 'active_flag'=> '0']);
     }
 }
